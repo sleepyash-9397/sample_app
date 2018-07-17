f8a2f7c8562247576891c6ada5a06d2421e1ef30
hang <hangtran9397@gmail.com>
2018-07-17 22:19:27 +0700

    Sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  add_flash_types :success, :warning, :danger, :info
 
   def hello
     render html: "hello, world!"
