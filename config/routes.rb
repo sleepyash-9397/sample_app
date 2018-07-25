74b55533bb91ffd02e08eda23bcd59fed26fbdba
hang <hangtran9397@gmail.com>
2018-07-25 10:57:05 +0700

    Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
+  get "password_resets/new"
+  get "password_resets/edit"
+
   get  "/signup", to: "users#new"
   resources :users
   resources :account_activations, only: :edit
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
